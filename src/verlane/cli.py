79b3390524238ce9b60b39e995cee272583e5259
Yosef Ahmed <YosefBesn123@gmail.com>
2026-08-09 19:45:58 +0300

    feat/initialize-cli (#1)
    * feat: initialize verlane cli
    
    * feat: add package version
    
    * feat: add typer cli entrypoint
    
    * chore: add uv lockfile
    
    * chore: add Python gitignore
    
    * chore: polish gitignore
    
    * feat: add short help option

---
+++ src/verlane/cli.py
@@ -0,0 +1,29 @@
+import typer
+
+from verlane import __version__
+
+app = typer.Typer(
+    add_completion=False,
+    context_settings={"help_option_names": ["-h", "--help"]},
+    invoke_without_command=True,
+)
+
+
+def version_callback(value: bool) -> None:
+    if value:
+        typer.echo(__version__)
+        raise typer.Exit()
+
+
+@app.callback()
+def main(
+    version: bool = typer.Option(
+        False,
+        "--version",
+        "-v",
+        callback=version_callback,
+        is_eager=True,
+        help="Show the Verlane version and exit.",
+    ),
+) -> None:
+    typer.echo("Not Implemented Yet")
